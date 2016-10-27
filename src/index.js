6c7085c367bbeabdcd0b7d7b57e86906d2b60255
Aurélien Luciani <luciani@ebi.ac.uk>
2016-10-27 10:09:11 +0100

    more readme + doesn't break if customElements is undefined

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _PdbPrints from './components/pdb-prints';
 import _PdbDataLoader from './components/pdb-data-loader';
-import DataLoader from 'data-loader/src/data-loader';
+import DataLoader from 'data-loader';
 
-customElements.define('pdb-prints', _PdbPrints);
-customElements.define('pdb-data-loader', _PdbDataLoader);
-customElements.define('data-loader', DataLoader);
+if (window.customElements) {
+  customElements.define('pdb-prints', _PdbPrints);
+  customElements.define('pdb-data-loader', _PdbDataLoader);
+  customElements.define('data-loader', DataLoader);
+}
 
 export const PdbDataLoader = _PdbDataLoader;
 export const PdbPrints = _PdbPrints;
